5bf8b2ae2de4d815bd74fbf82cd0b237d67ff634
José Cruz <jvsc@cin.ufpe.br>
2020-10-12 22:28:23 -0300

    refactor(Utils): changes from regex to constexpr

--- src/Utils/Utils.h
+++ src/Utils/Utils.h
@@ -2,21 +2,25 @@
 #define UTILS_H
 
 #include <QString>
-#include <QRegularExpression>
 
 namespace Utils {
   template <class T>
   QString nameOfType() {
-    const QRegularExpression regex("(.*with T = )(.*)(])",
-                                   QRegularExpression::NoPatternOption);
-    const QRegularExpressionMatch match(regex.match(__PRETTY_FUNCTION__));
-    const QStringList capturedTexts = match.capturedTexts();
-
-    if (capturedTexts.size() == 4) {
-      return capturedTexts[2];
+    constexpr std::size_t size = sizeof(__PRETTY_FUNCTION__) - 1;
+    QString ret;
+#if defined(__clang__)
+    for (std::size_t i = 33; i < size - 1; ++i) {
+      ret += __PRETTY_FUNCTION__[i];
     }
-
-    throw std::runtime_error("capture failed.");
+#elif defined(__GNUC__)
+    for (std::size_t i = 38; i < size - 1; ++i) {
+      ret += __PRETTY_FUNCTION__[i];
+    }
+#else
+    static_assert(std::false_type::value,
+                  "the chosen compiler is not allowed.");
+#endif
+    return ret;
   }
 
   template <class T>
