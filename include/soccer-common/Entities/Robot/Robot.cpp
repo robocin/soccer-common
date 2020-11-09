5d3c45999cfc4cd1719af6314d53767fa0c8d652
José Cruz <jvsc@cin.ufpe.br>
2020-11-09 18:37:07 -0300

    refactor(Robot): adds angle

--- include/soccer-common/Entities/Robot/Robot.cpp
+++ include/soccer-common/Entities/Robot/Robot.cpp
@@ -1,19 +1,25 @@
 #include "Robot.h"
 
-RawRobot::RawRobot(int _id, const QPointF& _position) :
+RawRobot::RawRobot(int _id, qreal _angle, const QPointF& _position) :
     RawEntity(_position),
-    m_id(_id) {
+    m_id(_id),
+    m_angle(_angle) {
 }
 
 int RawRobot::id() const {
   return m_id;
 }
 
+qreal RawRobot::angle() const {
+  return m_angle;
+}
+
 Robot::Robot(int _id,
+             qreal _angle,
              const QPointF& _position,
              const QPointF& _velocity,
              const QPointF& _acceleration) :
     RawEntity(_position),
     Entity(_position, _velocity, _acceleration),
-    RawRobot(_id, _position) {
+    RawRobot(_id, _angle, _position) {
 }
