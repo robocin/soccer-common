01e2af46ddfbbb30c670762cba1c897bc4d2f970
José Victor Cruz <jvsc@cin.ufpe.br>
2020-12-13 22:31:03 -0300

    refactor(ModuleBase): adds update method and RunInParallelAtEnd class (#30)
    * refactor(ModuleBase): adds update method and RunInParallelAtEnd class
    
    * refactor(RunInParallelAtEnd): renames parameter inside constructor

--- include/soccer-common/ModuleBase/ModuleBase.h
+++ include/soccer-common/ModuleBase/ModuleBase.h
@@ -20,6 +20,17 @@ class ModuleBase : public ModulePrivate {
   }
 
  protected:
+  class RunInParallelAtEnd {
+    ModuleBase* m;
+
+   public:
+    inline RunInParallelAtEnd(ModuleBase* self) : m(self) {
+    }
+    inline ~RunInParallelAtEnd() {
+      m->runInParallel();
+    }
+  };
+
   virtual void connectModules(const Modules* modules);
   virtual void buildParameters();
   virtual void init(const Modules* modules);
