4c540174061cede89a71234756de5bcc25c6efb2
José Cruz <jvsc@cin.ufpe.br>
2020-12-16 13:35:49 -0300

    refactor(ParameterType): fixes if constexpr order

--- include/soccer-common/Parameters/ParameterType/ParameterType.h
+++ include/soccer-common/Parameters/ParameterType/ParameterType.h
@@ -96,12 +96,12 @@ namespace Parameters {
         return Utils::quoted(MagicEnum::name(ref));
       } else if constexpr (std::is_same_v<T, bool>) {
         return ref ? "true" : "false";
-      } else if constexpr (std::is_same_v<T, QString>) {
-        return Utils::quoted(ref);
       } else if constexpr (std::is_floating_point_v<T>) {
         return QString::number(ref, 'f', 10);
-      } else {
+      } else if constexpr (std::is_arithmetic_v<T>) {
         return QString::number(ref);
+      } else if constexpr (std::is_same_v<T, QString>) {
+        return Utils::quoted(ref);
       }
     }
 
