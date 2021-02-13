4833f614b703335b6834993c40ae21f5a5718673
José Cruz <jvsc@cin.ufpe.br>
2021-02-12 22:11:29 -0300

    refactor(Robot): adds angular velocity

--- include/soccer-common/Entities/Robot/Robot.cpp
+++ include/soccer-common/Entities/Robot/Robot.cpp
@@ -18,8 +18,14 @@ Robot::Robot(int id,
              qreal angle,
              const QPointF& position,
              const QPointF& velocity,
-             const QPointF& acceleration) :
+             const QPointF& acceleration,
+             qreal angularVelocity) :
     RawEntity(position),
     Entity(position, velocity, acceleration),
-    RawRobot(id, angle, position) {
+    RawRobot(id, angle, position),
+    m_angularVelocity(angularVelocity) {
+}
+
+qreal Robot::angularVelocity() const {
+  return m_angularVelocity;
 }
--- include/soccer-common/Entities/Robot/Robot.h
+++ include/soccer-common/Entities/Robot/Robot.h
@@ -15,12 +15,17 @@ class RawRobot : virtual public RawEntity {
 };
 
 class Robot : public Entity, public RawRobot {
+  qreal m_angularVelocity;
+
  public:
   Robot(int id,
         qreal angle,
         const QPointF& position,
         const QPointF& velocity,
-        const QPointF& acceleration);
+        const QPointF& acceleration,
+        qreal angularVelocity);
+
+  qreal angularVelocity() const;
 };
 
 #endif // SOCCER_COMMON_ROBOT_H
