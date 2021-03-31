506ea800ecce87a2b73599d1808b4da4479129c2
José Cruz <jvsc@cin.ufpe.br>
2021-03-31 18:03:25 -0300

    refactor(RobotDetails): minor chages

--- include/soccer-common/gui/Widgets/RobotDetails/RobotDetails.cpp
+++ include/soccer-common/gui/Widgets/RobotDetails/RobotDetails.cpp
@@ -3,15 +3,16 @@
 
 #include "soccer-common/gui/MainWindow/MainWindowMenuBar/MainWindowMenuBar.h"
 
-RobotDetails::RobotDetails(int index, const QString &robotsPatternPath, QWidgetWith<WidgetSettings> parent) :
+RobotDetails::RobotDetails(int index,
+                           const QString& robotsPatternPath,
+                           QWidgetWith<WidgetSettings> parent) :
     QWidget(parent),
     WidgetSettings(this, parent),
     ui(new Ui::RobotDetails) {
   ui->setupUi(this);
   showRobotNumber(index);
   showRobotPattern(index, robotsPatternPath, "blue");
-  statusInfraRed(true); //trocar
-
+  statusInfraRed(true); // trocar
 }
 
 RobotDetails::~RobotDetails() {
@@ -29,38 +30,39 @@ void RobotDetails::showRobotNumber(int number) {
   scene = new QGraphicsScene(this);
   ui->robotID->setScene(scene);
   QPixmap pixmap(":/robotID.png");
-  QGraphicsPixmapItem *id = new QGraphicsPixmapItem(pixmap);
+  QGraphicsPixmapItem* id = new QGraphicsPixmapItem(pixmap);
   scene->addItem(id);
   scene->setSceneRect((number * 70), 0, 70, 70);
 }
 
-void RobotDetails::statusInfraRed(bool isOn){
-    QGraphicsScene* scene;
-    scene = new QGraphicsScene(this);
-    ui->irTitle->setScene(scene);
-    QGraphicsPixmapItem *id;
-    QPixmap pixmapOn(":/led_on.png");
-    QPixmap pixmapOff(":/led_off.png");
-    if(isOn){
-        id = new QGraphicsPixmapItem(pixmapOn);
-    }
-    else id = new QGraphicsPixmapItem(pixmapOff);
-    scene->addItem(id);
-    scene->setSceneRect(0,0,32,35);
+void RobotDetails::statusInfraRed(bool isOn) {
+  QGraphicsScene* scene;
+  scene = new QGraphicsScene(this);
+  ui->irTitle->setScene(scene);
+  QGraphicsPixmapItem* id;
+  QPixmap pixmapOn(":/led_on.png");
+  QPixmap pixmapOff(":/led_off.png");
+  if (isOn) {
+    id = new QGraphicsPixmapItem(pixmapOn);
+  } else
+    id = new QGraphicsPixmapItem(pixmapOff);
+  scene->addItem(id);
+  scene->setSceneRect(0, 0, 32, 35);
 }
 
-void RobotDetails::showRobotPattern(int number, const QString &robotsPatternPath, QString color){
-    QGraphicsScene* scene;
-    QPixmap pixmap(robotsPatternPath);
-    QColor background(color);
-    QBrush brush(background);
-    scene = new QGraphicsScene(this);
-    ui->robotPattern->setScene(scene);
-    QGraphicsPixmapItem *id = new QGraphicsPixmapItem(pixmap);
-    scene->setBackgroundBrush(brush);
-    scene->addItem(id);
-    scene->setSceneRect((number * 70), 0, 70, 70);
-
+void RobotDetails::showRobotPattern(int number,
+                                    const QString& robotsPatternPath,
+                                    QString color) {
+  QGraphicsScene* scene;
+  QPixmap pixmap(robotsPatternPath);
+  QColor background(color);
+  QBrush brush(background);
+  scene = new QGraphicsScene(this);
+  ui->robotPattern->setScene(scene);
+  QGraphicsPixmapItem* id = new QGraphicsPixmapItem(pixmap);
+  scene->setBackgroundBrush(brush);
+  scene->addItem(id);
+  scene->setSceneRect((number * 70), 0, 70, 70);
 }
 
 /*
