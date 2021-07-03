2c0c3cdb940b39eadd06f4d6083448a31656e4aa
José Cruz <jvsc@cin.ufpe.br>
2021-07-03 01:10:03 -0300

    refactor(ColorTeamWidget): implements correctly and rem WidgetSettings

--- include/soccer-common/Gui/Widgets/ColorTeamWidget/ColorTeamWidget.cpp
+++ include/soccer-common/Gui/Widgets/ColorTeamWidget/ColorTeamWidget.cpp
@@ -1,12 +1,9 @@
 #include "ColorTeamWidget.h"
 #include "ui_ColorTeamWidget.h"
-#include <QtCore>
-#include <QtWidgets>
 #include "soccer-common/Gui/MainWindow/MainWindowMenuBar/MainWindowMenuBar.h"
 
-ColorTeamWidget::ColorTeamWidget(QWidgetWith<WidgetSettings, MenuBarOptions> parent) :
+ColorTeamWidget::ColorTeamWidget(QWidgetWith<MenuBarOptions> parent) :
     QWidget(parent),
-    WidgetSettings(this, parent),
     MenuBarOptions(parent),
     ui(new Ui::ColorTeamWidget),
     viewAction(Factory::toggleViewAction(this)) {
@@ -17,20 +14,14 @@ ColorTeamWidget::~ColorTeamWidget() {
   delete ui;
 }
 
-void ColorTeamWidget::setColor(QColor color) {
-  int r, g, b;
-  color.getRgb(&r, &g, &b);
-  ui->Color->setText(QString::number(r) + " " + QString::number(g) + " " + QString::number(b));
+void ColorTeamWidget::setColor(const QColor& color) {
+  QGraphicsScene* scene;
+  scene = new QGraphicsScene(this);
+  scene->setBackgroundBrush(color);
+  ui->colorView->setScene(scene);
+  ui->colorView->setBackgroundBrush(scene->backgroundBrush().color());
 }
 
-void ColorTeamWidget::writeLocalSettings(QSettings& settings) {
-  settings.setValue("teamColor", ui->Color->text());
-}
-
-void ColorTeamWidget::loadLocalSettings(const QSettings& settings) {
-  QString savedColor = settings.value("teamColor").value<QString>();
-  setColor(savedColor);
-}
 void ColorTeamWidget::putWidgetActions(MainWindowMenuBar& menubar) {
-  menubar["View"]["Informations"].addAction(viewAction);
+  menubar["View"]["Information"].addAction(viewAction);
 }
