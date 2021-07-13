e879313e32ff46eb13fef8c32133ffa08e03b7b8
José Cruz <jvsc@cin.ufpe.br>
2021-07-13 14:46:59 -0300

    feat(Robots): creates the class

--- include/soccer-common/Entities/Entities.h
+++ include/soccer-common/Entities/Entities.h
@@ -4,5 +4,6 @@
 #include "Entity/Entity.h"
 #include "Ball/Ball.h"
 #include "Robot/Robot.h"
+#include "Robots/Robots.h"
 
 #endif // SOCCER_COMMON_ENTITIES_H
---
+++ include/soccer-common/Entities/Robots/Robots.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jose-cruz on 12/07/2021.
+//
+
+#include "Robots.h"
