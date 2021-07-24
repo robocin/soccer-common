51a86b0740efc6bfdb01e9eff82bd9dfbcdd8d4f
José Cruz <jvsc@cin.ufpe.br>
2021-07-24 02:15:28 -0300

    feat(Utils/meta/is_in): creates the file and adds fundamental methods

---
+++ include/soccer-common/Utils/meta/is_in/is_in.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jose-cruz on 23/07/2021.
+//
+
+#include "is_in.h"
--- include/soccer-common/Utils/meta/meta.h
+++ include/soccer-common/Utils/meta/meta.h
@@ -3,5 +3,6 @@
 
 #include "soccer-common/Utils/meta/meta_package/meta_package.h"
 #include "static_block/static_block.h"
+#include "is_in/is_in.h"
 
 #endif // SOCCER_COMMON_META_H
