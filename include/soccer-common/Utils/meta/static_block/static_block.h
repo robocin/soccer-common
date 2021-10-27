b8887bf8c995f3824b21d03d24d0312c79504536
José Cruz <jvsc@cin.ufpe.br>
2021-10-26 22:18:11 -0300

    refactor(static_block): renames macros

--- include/soccer-common/Utils/meta/static_block/static_block.h
+++ include/soccer-common/Utils/meta/static_block/static_block.h
@@ -6,7 +6,7 @@
 namespace __rc_meta { // NOLINT(bugprone-reserved-identifier)
   struct [[nodiscard]] [[maybe_unused]] static_block_tag {
     template <class Functor>
-    inline explicit static_block_tag(Functor && f) {
+    inline explicit static_block_tag(Functor&& f) {
       std::forward<Functor>(f)();
     }
     ~static_block_tag() = default;
@@ -16,17 +16,19 @@ namespace __rc_meta { // NOLINT(bugprone-reserved-identifier)
     static_block_tag& operator=(const static_block_tag&) = delete;
 
     // disable_move:
-    static_block_tag(static_block_tag &&) = delete;
+    static_block_tag(static_block_tag&&) = delete;
     static_block_tag& operator=(static_block_tag&&) = delete;
   };
 } // namespace __rc_meta
 
-#define __RC_TOKEN_PASTING__(A, B) __RC_TOKEN_PASTING_IMPL__(A, B)
-#define __RC_TOKEN_PASTING_IMPL__(A, B) A##B
+#define __RC_STATIC_BLOCK_TOKEN_PASTING__(A, B) __RC_STATIC_BLOCK_TOKEN_PASTING_IMPL__(A, B)
+#define __RC_STATIC_BLOCK_TOKEN_PASTING_IMPL__(A, B) A##B
 
 #define __static_block_declaration__                                                               \
-  inline static const auto __RC_TOKEN_PASTING__(                                                   \
-      __RC_TOKEN_PASTING__(__RC_TOKEN_PASTING__(static_block_tag_, __COUNTER__), _at_line_),       \
+  inline static const auto __RC_STATIC_BLOCK_TOKEN_PASTING__(                                      \
+      __RC_STATIC_BLOCK_TOKEN_PASTING__(                                                           \
+          __RC_STATIC_BLOCK_TOKEN_PASTING__(static_block_tag_, __COUNTER__),                       \
+          _at_line_),                                                                              \
       __LINE__) = (__rc_meta::static_block_tag)[]()->void
 
 #define static_block __static_block_declaration__ // NOLINT(cert-err58-cpp)
