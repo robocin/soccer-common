63f3b1d4587595fbdf326dc6a60eff936b0b98f7
José Cruz <jvsc@cin.ufpe.br>
2021-12-23 21:11:10 -0300

    test(TestField): syncs with src changes

--- test/TestField/TestField.cpp
+++ test/TestField/TestField.cpp
@@ -91,6 +91,21 @@ void TestField::test_field_constexpr_withVSS3x3Values() {
   static_assert(field.allyPenaltyAreaCornerTop() == QPoint(-60, +35));
   static_assert(field.enemyPenaltyAreaCornerTop() == QPoint(+60, +35));
 
+  static_assert(field.leftPenaltyAreaGoalCornerTop() == QPoint(-75, +35));
+  static_assert(field.rightPenaltyAreaGoalCornerTop() == QPoint(+75, +35));
+  static_assert(field.allyPenaltyAreaGoalCornerTop() == QPoint(-75, +35));
+  static_assert(field.enemyPenaltyAreaGoalCornerTop() == QPoint(+75, +35));
+
+  static_assert(field.leftPenaltyAreaGoalCornerBottom() == QPoint(-75, -35));
+  static_assert(field.rightPenaltyAreaGoalCornerBottom() == QPoint(+75, -35));
+  static_assert(field.allyPenaltyAreaGoalCornerBottom() == QPoint(-75, -35));
+  static_assert(field.enemyPenaltyAreaGoalCornerBottom() == QPoint(+75, -35));
+
+  static_assert(Field::leftDirection() == QPoint(-1, 0));
+  static_assert(Field::rightDirection() == QPoint(+1, 0));
+  static_assert(field.attackDirection() == QPoint(+1, 0));
+  static_assert(field.defendDirection() == QPoint(-1, 0));
+
   static_assert(!field.leftGoalContains(QPoint(-85, -20)));
   static_assert(!field.leftGoalContains(QPoint(-85, -19)));
   static_assert(field.leftGoalContains(QPoint(-84, -19)));
